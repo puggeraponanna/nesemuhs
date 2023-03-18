561bff2b619123c6a14cb756d12e5d0cbf2a9ab9
puggeraponanna <ponannapuggera@gmail.com>
2023-03-18 23:55:23 +0530

    feat: adds support different memory addressing modes

--- src/NesEmu/Cpu.hs
+++ src/NesEmu/Cpu.hs
@@ -6,48 +6,114 @@ import           Data.Word
 data Cpu = Cpu
   { registerA      :: !Word8,
     registerX      :: !Word8,
+    registerY      :: !Word8,
     status         :: !Word8,
     programCounter :: !Word16,
     memory         :: ![Word8]
   }
   deriving (Show)
 
+reset :: Cpu -> Cpu
+reset cpu =
+  cpu
+    { registerA = 0,
+      registerX = 0,
+      status = 0,
+      programCounter = memoryRead16 cpu 0xFFFC
+    }
+
 newCpu :: Cpu
 newCpu =
   Cpu
     { registerA = 0,
       registerX = 0,
+      registerY = 0,
       status = 0,
       programCounter = 0,
       memory = replicate 0xFFFF 0
     }
 
-loadAndRun :: [Word8] -> Cpu
-loadAndRun = run . loadProgram
+loadAndRun :: Cpu -> [Word8] -> Cpu
+loadAndRun cpu = run . loadProgram cpu
 
-loadProgram :: [Word8] -> Cpu
-loadProgram pgm =
-  Cpu
-    { registerA = 0,
-      registerX = 0,
-      status = 0,
-      programCounter = 0x8000,
-      memory = replicate 0x8000 0 ++ pgm ++ replicate l 0
-    }
+loadProgram :: Cpu -> [Word8] -> Cpu
+loadProgram cpu pgm =
+  let cpu' =
+        cpu
+          { memory = take 0x8000 (memory cpu) ++ pgm ++ drop l (memory cpu)
+          }
+   in reset $ memoryWrite16 cpu' 0xFFFC 0x8000
   where
-    l = 0xFFFF - length pgm - 0x8000
+    l = length pgm + 0x8000
 
 run :: Cpu -> Cpu
 run cpu =
   case opcode of
-    0x00 -> brk cpu
-    0xAA -> run . incPc $ tax cpu
-    0xE8 -> run . incPc $ inx cpu
-    0xA9 -> run . incPc $ lda cpu (memoryRead cpu $ programCounter cpu + 1)
+    0x00 -> brk (incPc 1 cpu)
+    0xAA -> run $ tax (incPc 1 cpu)
+    0xE8 -> run $ inx (incPc 1 cpu)
+    0xA9 -> run . incPc 1 $ lda (incPc 1 cpu) Immediate
+    0xA5 -> run . incPc 1 $ lda (incPc 1 cpu) ZeroPage
+    0xB5 -> run . incPc 1 $ lda (incPc 1 cpu) ZeroPageX
+    0xAD -> run . incPc 2 $ lda (incPc 1 cpu) Absolute
+    0xBD -> run . incPc 2 $ lda (incPc 1 cpu) AbsoluteX
+    0xB9 -> run . incPc 2 $ lda (incPc 1 cpu) AbsoluteY
+    0xA1 -> run . incPc 1 $ lda (incPc 1 cpu) IndirectX
+    0xB1 -> run . incPc 1 $ lda (incPc 1 cpu) IndirectY
+    0x85 -> run . incPc 1 $ sta (incPc 1 cpu) ZeroPage
+    0x95 -> run . incPc 1 $ sta (incPc 1 cpu) ZeroPageX
+    0x8D -> run . incPc 1 $ sta (incPc 2 cpu) Absolute
+    0x9D -> run . incPc 1 $ sta (incPc 2 cpu) AbsoluteX
+    0x99 -> run . incPc 1 $ sta (incPc 2 cpu) AbsoluteY
+    0x81 -> run . incPc 1 $ sta (incPc 1 cpu) IndirectX
+    0x91 -> run . incPc 1 $ sta (incPc 1 cpu) IndirectY
     _    -> undefined
   where
     opcode = memoryRead cpu (programCounter cpu)
-    incPc a = a {programCounter = programCounter a + 1}
+    incPc i a = a {programCounter = programCounter a + i}
+
+data AddressingMode
+  = Immediate
+  | ZeroPage
+  | ZeroPageX
+  | ZeroPageY
+  | Absolute
+  | AbsoluteX
+  | AbsoluteY
+  | IndirectX
+  | IndirectY
+  | NoneAddressing
+  deriving (Show)
+
+getOperandAddress :: Cpu -> AddressingMode -> Word16
+getOperandAddress cpu Immediate = programCounter cpu
+getOperandAddress cpu ZeroPage = fromIntegral $ memoryRead cpu (programCounter cpu)
+getOperandAddress cpu Absolute = memoryRead16 cpu (programCounter cpu)
+getOperandAddress cpu ZeroPageX =
+  let pos = memoryRead cpu (programCounter cpu)
+   in fromIntegral pos + fromIntegral (registerX cpu)
+getOperandAddress cpu ZeroPageY =
+  let pos = memoryRead cpu (programCounter cpu)
+   in fromIntegral pos + fromIntegral (registerY cpu)
+getOperandAddress cpu AbsoluteX =
+  let base = memoryRead16 cpu (programCounter cpu)
+   in base + fromIntegral (registerX cpu)
+getOperandAddress cpu AbsoluteY =
+  let base = memoryRead16 cpu (programCounter cpu)
+   in base + fromIntegral (registerY cpu)
+getOperandAddress cpu IndirectX =
+  let base = memoryRead cpu (programCounter cpu)
+      ptr = base + registerX cpu
+      lo = fromIntegral $ memoryRead cpu (fromIntegral ptr)
+      hi = fromIntegral $ memoryRead cpu (fromIntegral ptr + 1)
+   in (hi `shiftL` 8) + lo
+getOperandAddress cpu IndirectY =
+  let base = memoryRead cpu (programCounter cpu)
+      lo = memoryRead cpu (fromIntegral base)
+      hi = memoryRead cpu (fromIntegral base + 1)
+      deref_base = (fromIntegral hi `shiftL` 8) + fromIntegral lo
+   in deref_base + fromIntegral (registerY cpu)
+getOperandAddress _ NoneAddressing = undefined
 
 -- Set Flags
 data Flag
@@ -79,23 +145,42 @@ setVF = setFlag Overflow . (== 0xFF)
 memoryWrite :: Cpu -> Word16 -> Word8 -> Cpu
 memoryWrite cpu addr val =
   cpu
-    { memory = take addrI mem ++ [val] ++ drop (addrI + 1) mem
+    { memory = take addr' mem ++ [val] ++ drop (addr' + 1) mem
     }
   where
     mem = memory cpu
-    addrI = fromIntegral addr :: Int
+    addr' = fromIntegral addr :: Int
+
+memoryWrite16 :: Cpu -> Word16 -> Word16 -> Cpu
+memoryWrite16 cpu addr val = memoryWrite (memoryWrite cpu addr lo) (addr + 1) hi
+  where
+    lo = fromIntegral $ (val .&. 0xFF)
+    hi = fromIntegral $ val `shiftR` 8
 
 memoryRead :: Cpu -> Word16 -> Word8
-memoryRead cpu addr = memory cpu !! (fromIntegral addr :: Int)
+memoryRead cpu addr = memory cpu !! fromIntegral addr
+
+memoryRead16 :: Cpu -> Word16 -> Word16
+memoryRead16 cpu addr = (hi `shiftL` 8) + lo
+  where
+    lo = fromIntegral $ memoryRead cpu addr
+    hi = fromIntegral $ memoryRead cpu (addr + 1)
 
 -- Operations
-lda :: Cpu -> Word8 -> Cpu
-lda cpu val =
-  cpu
-    { registerA = val,
-      programCounter = programCounter cpu + 1,
-      status = setZF val $ setNF val $ status cpu
-    }
+lda :: Cpu -> AddressingMode -> Cpu
+lda cpu addrMode =
+  let addr = getOperandAddress cpu addrMode
+      val = memoryRead cpu addr
+   in cpu
+        { registerA = val,
+          status = setZF val $ setNF val $ status cpu
+        }
+
+sta :: Cpu -> AddressingMode -> Cpu
+sta cpu addrMode =
+  let addr = getOperandAddress cpu addrMode
+      val = registerA cpu
+   in memoryWrite cpu addr val
 
 tax :: Cpu -> Cpu
 tax cpu =
--- tests/CpuSpec.hs
+++ tests/CpuSpec.hs
@@ -1,51 +1,91 @@
 module CpuSpec (spec) where
 
 import           Data.Bits
-import           GHC.IO.Handle.Text (memcpy)
 import           NesEmu.Cpu
 import           Test.Hspec
 
 spec :: Spec
 spec =
   describe "CPU" $ do
-    describe "LDA IMM" $ do
-      it "Should load data to Register A from Immediate address" $ do
-        let cpu = loadAndRun [0xA9, 0x11, 0x00]
-        let zf = testBit (status cpu) 1
-        let nf = testBit (status cpu) 7
+    describe "LDA"$ do
+      describe "Immediate" $ do
+        it "Should load data to Register A from Immediate address" $ do
+          let cpu = loadAndRun newCpu [0xA9, 0x11, 0x00]
+          let zf = testBit (status cpu) 1
+          let nf = testBit (status cpu) 7
 
-        registerA cpu `shouldBe` 0x11
-        zf `shouldBe` False
-        nf `shouldBe` False
+          registerA cpu `shouldBe` 0x11
+          zf `shouldBe` False
+          nf `shouldBe` False
 
-      it "Should set the Zero flag if result is zero" $ do
-        let cpu = loadAndRun [0xA9, 0x00, 0x00]
-        let zf = testBit (status cpu) 1
-        let nf = testBit (status cpu) 7
+        it "Should set the Zero flag if result is zero" $ do
+          let cpu = loadAndRun newCpu [0xA9, 0x00, 0x00]
+          let zf = testBit (status cpu) 1
+          let nf = testBit (status cpu) 7
 
-        zf `shouldBe` True
-        nf `shouldBe` False
+          zf `shouldBe` True
+          nf `shouldBe` False
 
-      it "Should set the Negative flag if result is negative" $ do
-        let cpu = loadAndRun [0xA9, 0x80, 0x00]
-        let zf = testBit (status cpu) 1
-        let nf = testBit (status cpu) 7
+        it "Should set the Negative flag if result is negative" $ do
+          let cpu = loadAndRun newCpu [0xA9, 0x80, 0x00]
+          let zf = testBit (status cpu) 1
+          let nf = testBit (status cpu) 7
 
-        zf `shouldBe` False
-        nf `shouldBe` True
+          zf `shouldBe` False
+          nf `shouldBe` True
+
+      describe "ZeroPage" $ do
+        it "Should load data to Register A from Zero Page address" $ do
+          let initCpu = memoryWrite newCpu 0x10 0x55
+          let cpu = loadAndRun initCpu [0xA5, 0x10, 0x00]
+          let zf = testBit (status cpu) 1
+          let nf = testBit (status cpu) 7
+
+          registerA cpu `shouldBe` 0x55
+          zf `shouldBe` False
+          nf `shouldBe` False
+
+      describe "ZeroPage X offset" $ do
+        it "Should load data to Register A from Zero Page address with X offset" $ do
+          let initCpu = memoryWrite newCpu 0x12 0x55
+          let cpu = loadAndRun initCpu [0xA9, 0x02, 0xAA, 0xB5, 0x10, 0x00]
+          let zf = testBit (status cpu) 1
+          let nf = testBit (status cpu) 7
+
+          registerA cpu `shouldBe` 0x55
+          zf `shouldBe` False
+          nf `shouldBe` False
+
+      describe "Absolute" $ do
+        it "Should load data to Register A from Absolute address" $ do
+          let initCpu = memoryWrite newCpu 0x12 0x55
+          let cpu = loadAndRun initCpu [0xAD, 0x12, 0x00]
+          let zf = testBit (status cpu) 1
+          let nf = testBit (status cpu) 7
+
+          registerA cpu `shouldBe` 0x55
+          zf `shouldBe` False
+          nf `shouldBe` False
+
+    describe "STA" $ do
+      describe "ZeroPage" $ do
+        it "Should write data from Register A to Zero Page address" $ do
+          let cpu = loadAndRun newCpu [0xA9, 0x55, 0x85, 0x10, 0x00]
+
+          memoryRead cpu 0x10 `shouldBe` 0x55
 
     describe "TAX" $ do
       it "Should transfer data from Register A to Register X" $ do
-        let cpu = loadAndRun [0xA9, 0x45, 0xAA, 0x00]
+        let cpu = loadAndRun newCpu [0xA9, 0x45, 0xAA, 0x00]
         let zf = testBit (status cpu) 1
         let nf = testBit (status cpu) 7
 
-        registerX cpu `shouldBe` 0x45
+        registerX (tax cpu) `shouldBe` 0x45
         zf `shouldBe` False
         nf `shouldBe` False
 
       it "Should set the Zero flag if result is zero" $ do
-        let cpu = loadAndRun [0xAA, 0x00]
+        let cpu = loadAndRun newCpu [0xAA, 0x00]
         let zf = testBit (status cpu) 1
         let nf = testBit (status cpu) 7
 
@@ -53,7 +93,7 @@ spec =
         nf `shouldBe` False
 
       it "Should set the Negative flag if result is negative" $ do
-        let cpu = loadAndRun [0xA9, 0x91, 0xAA, 0x00]
+        let cpu = loadAndRun newCpu [0xA9, 0x91, 0xAA, 0x00]
         let zf = testBit (status cpu) 1
         let nf = testBit (status cpu) 7
 
@@ -62,7 +102,7 @@ spec =
 
     describe "INX" $ do
       it "Should increment the value in Register X" $ do
-        let cpu = loadAndRun [0xA9, 0x11, 0xAA, 0xE8, 0x00]
+        let cpu = loadAndRun newCpu [0xA9, 0x11, 0xAA, 0xE8, 0x00]
 
         let zf = testBit (status cpu) 1
         let nf = testBit (status cpu) 7
@@ -74,7 +114,7 @@ spec =
         vf `shouldBe` False
 
       it "Should increment the value in Register X and set the Overflow flag" $ do
-        let cpu = loadAndRun [0xA9, 0xFF, 0xAA, 0xE8, 0x00]
+        let cpu = loadAndRun newCpu [0xA9, 0xFF, 0xAA, 0xE8, 0x00]
 
         let zf = testBit (status cpu) 1
         let nf = testBit (status cpu) 7
@@ -84,19 +124,3 @@ spec =
         zf `shouldBe` True
         nf `shouldBe` False
         vf `shouldBe` True
-
-    describe "Memory Access" $ do
-      it "SHould successfully write into memory" $ do
-        let cpu = memoryWrite newCpu 0x01 0xAD
-
-        memory cpu !! 0x01 `shouldBe` 0xAD
-        length (memory cpu) `shouldBe` 0xFFFF
-
-      it "Should successfully read from memory" $ do
-        let cpu =
-              newCpu
-                { memory = [0x12, 0x34]
-                }
-
-        let dt = memoryRead cpu 0x0000
-        dt `shouldBe` 0x12

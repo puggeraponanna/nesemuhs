8546711a34962bc6bbadab151508ef8ef266fe4f
puggeraponanna <ponannapuggera@gmail.com>
2023-08-20 19:42:02 +0530

    adds tests for memory

--- tests/NesEmuTest/Cpu/Memory.hs
+++ tests/NesEmuTest/Cpu/Memory.hs
@@ -3,6 +3,7 @@ module NesEmuTest.Cpu.Memory where
 import           NesEmu.Cpu
 import           NesEmu.Cpu.Memory
 import           NesEmu.Cpu.Types
+import           NesEmu.Cpu.Opcodes
 import           Test.Hspec
 
 spec :: Spec
@@ -19,21 +20,62 @@ spec = do
       let cpu' = memoryWrite cpu 0 0xAB
       memory cpu' `shouldBe` [0xAB, 0x00]
 
-  describe "ZeroPage addressing" $ do
-    it "reads from zeropage address" $ do
-      let cpu = loadProgram newCpu [0xA5, 0x10]
-      let cpu' = memoryWrite cpu 0x10 0xAB
-      registerA cpu' `shouldBe` 0xAB
+  describe "memoryWrite16" $ do
+    it "writes a 16-bit value across two bytes" $ do
+      let cpu = newCpu {memory = replicate 0xFFFF 0}
+      let cpu' = memoryWrite16 cpu 0x1234 0xABCD
+      memoryRead cpu' 0x1234 `shouldBe` 0xCD
+      memoryRead cpu' 0x1235 `shouldBe` 0xAB
 
-  describe "Absolute addressing" $ do
-    it "reads from absolute address" $ do
-      let cpu = loadProgram newCpu [0xAD, 0x34, 0x12]
-      let cpu' = memoryWrite cpu 0x1234 0xCD
-      registerA cpu `shouldBe` 0xCD
+  describe "memoryRead16" $ do
+    it "reads a 16-bit value from two bytes" $ do
+      let cpu = newCpu {memory = [0xCD, 0xAB]}
+      memoryRead16 cpu 0 `shouldBe` 0xABCD
 
-  describe "IndirectX addressing" $ do
-    it "reads from dereferenced address offset by X" $ do
-      let cpu = loadProgram newCpu [0xA2, 0x05, 0xA1, 0x10]
-      let cpu' = memoryWrite cpu 0x15 0x34
-      let cpu' = memoryWrite cpu 0x1234 0xAB
-      registerA cpu `shouldBe` 0xAB
+  describe "getOperandAddress" $ do
+    describe "Immediate" $ do
+      it "returns the program counter" $ do
+        let cpu = newCpu {programCounter = 0x1000}
+        getOperandAddress cpu Immediate `shouldBe` 0x1000
+
+    describe "ZeroPage" $ do
+      it "returns the address from zeropage" $ do
+        let cpu = newCpu {memory = [0x34, 0x12]}
+        getOperandAddress cpu ZeroPage `shouldBe` 0x34
+
+    describe "ZeroPageX" $ do
+      it "returns zeropage + X register" $ do
+        let cpu = newCpu { registerX = 0x05, memory = [0x30]}
+        getOperandAddress cpu ZeroPageX `shouldBe` 0x35
+
+    describe "ZeroPageY" $ do
+      it "returns zeropage + Y register" $ do
+        let cpu = newCpu { registerY = 0x05, memory = [0x30]}
+        getOperandAddress cpu ZeroPageY `shouldBe` 0x35
+
+    describe "Absolute" $ do
+      it "returns the 16-bit address" $ do
+        let cpu = newCpu { memory = [0x34, 0x12]}
+        getOperandAddress cpu Absolute `shouldBe` 0x1234
+
+    describe "AbsoluteX" $ do
+      it "returns absolute address + X register" $ do
+        let cpu = newCpu { registerX = 0x05, memory = [0x34, 0x12]}
+        getOperandAddress cpu AbsoluteX `shouldBe` 0x1239
+
+    describe "AbsoluteY" $ do
+      it "returns absolute address + Y register" $ do
+        let cpu = newCpu { registerY = 0x05, memory = [0x34, 0x12]}
+        getOperandAddress cpu AbsoluteY `shouldBe` 0x1239
+
+    describe "IndirectX" $ do
+      it "returns dereferenced address + X register" $ do
+        let cpu = newCpu { registerX = 0x05, memory = [0,0,0,0,0, 0x34, 0x12]}
+        getOperandAddress cpu IndirectX `shouldBe` 0x1234
+
+    -- TODO: There might be some problem with this
+    describe "IndirectY" $ do
+      it "returns dereferenced address + Y register" $ do
+        let cpu = newCpu { registerY = 0x05, memory = [0, 0x34, 0x12]} 
+        getOperandAddress cpu IndirectY `shouldBe` 0x3405
+  

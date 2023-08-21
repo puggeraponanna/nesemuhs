e8d74c7872cdf609d95d2834d71b9db20b7804cf
puggeraponanna <ponannapuggera@gmail.com>
2023-08-21 22:39:19 +0530

    chore: refactor

--- src/NesEmu/Cpu.hs
+++ src/NesEmu/Cpu.hs
@@ -41,87 +41,14 @@ loadProgram cpu pgm =
     l = length pgm + 0x8000
 
 run :: Cpu -> Cpu
-run cpu =
+run cpu = do
+    let opcode = memoryRead cpu (programCounter cpu)
+        cpu' = incrementPc cpu 1
+        cpu'' = getOperation opcode cpu'
+        cpu''' = incrementPc cpu'' $ getCycleCount opcode
     case opcode of
-        BRK          -> brk (incPc 1 cpu)
-        TAX          -> run . incPc 0 $ tax (incPc 1 cpu)
-        INX          -> run . incPc 0 $ inx (incPc 1 cpu)
-        LDAImmediate -> run . incPc 1 $ lda (incPc 1 cpu) Immediate
-        LDAZeroPage  -> run . incPc 1 $ lda (incPc 1 cpu) ZeroPage
-        LDAZeroPageX -> run . incPc 1 $ lda (incPc 1 cpu) ZeroPageX
-        LDAAbsolute  -> run . incPc 2 $ lda (incPc 1 cpu) Absolute
-        LDAAbsoluteX -> run . incPc 2 $ lda (incPc 1 cpu) AbsoluteX
-        LDAAbsoluteY -> run . incPc 2 $ lda (incPc 1 cpu) AbsoluteY
-        LDAIndirectX -> run . incPc 1 $ lda (incPc 1 cpu) IndirectX
-        LDAIndirectY -> run . incPc 1 $ lda (incPc 1 cpu) IndirectY
-        LDXImmediate -> run . incPc 1 $ ldx (incPc 1 cpu) Immediate
-        LDXZeroPage  -> run . incPc 1 $ ldx (incPc 1 cpu) ZeroPage
-        LDXZeroPageY -> run . incPc 1 $ ldx (incPc 1 cpu) ZeroPageY
-        LDXAbsolute  -> run . incPc 2 $ ldx (incPc 1 cpu) Absolute
-        LDXAbsoluteY -> run . incPc 2 $ ldx (incPc 1 cpu) AbsoluteY
-        STAZeroPage  -> run . incPc 1 $ sta (incPc 1 cpu) ZeroPage
-        STAZeroPageX -> run . incPc 1 $ sta (incPc 1 cpu) ZeroPageX
-        STAAbsolute  -> run . incPc 2 $ sta (incPc 1 cpu) Absolute
-        STAAbsoluteX -> run . incPc 2 $ sta (incPc 1 cpu) AbsoluteX
-        STAAbsoluteY -> run . incPc 2 $ sta (incPc 1 cpu) AbsoluteY
-        STAIndirectX -> run . incPc 1 $ sta (incPc 1 cpu) IndirectX
-        STAIndirectY -> run . incPc 1 $ sta (incPc 1 cpu) IndirectY
-        ADCImmediate -> run . incPc 1 $ adc (incPc 1 cpu) Immediate
-        ADCZeroPage  -> run . incPc 1 $ adc (incPc 1 cpu) ZeroPage
-        ADCZeroPageX -> run . incPc 1 $ adc (incPc 1 cpu) ZeroPageX
-        ADCAbsolute  -> run . incPc 2 $ adc (incPc 1 cpu) Absolute
-        ADCAbsoluteX -> run . incPc 2 $ adc (incPc 1 cpu) AbsoluteX
-        ADCAbsoluteY -> run . incPc 2 $ adc (incPc 1 cpu) AbsoluteY
-        ADCIndirectX -> run . incPc 1 $ adc (incPc 1 cpu) IndirectX
-        ADCIndirectY -> run . incPc 1 $ adc (incPc 1 cpu) IndirectY
-        SBCImmediate -> run . incPc 1 $ sbc (incPc 1 cpu) Immediate
-        SBCZeroPage  -> run . incPc 1 $ sbc (incPc 1 cpu) ZeroPage
-        SBCZeroPageX -> run . incPc 1 $ sbc (incPc 1 cpu) ZeroPageX
-        SBCAbsolute  -> run . incPc 2 $ sbc (incPc 1 cpu) Absolute
-        SBCAbsoluteX -> run . incPc 2 $ sbc (incPc 1 cpu) AbsoluteX
-        SBCAbsoluteY -> run . incPc 2 $ sbc (incPc 1 cpu) AbsoluteY
-        SBCIndirectX -> run . incPc 1 $ sbc (incPc 1 cpu) IndirectX
-        SBCIndirectY -> run . incPc 1 $ sbc (incPc 1 cpu) IndirectY
-        _            -> error "Unknown opcode"
-  where
-    opcode = memoryRead cpu (programCounter cpu)
-    incPc i a = a{programCounter = programCounter a + i}
+        BRK -> cpu'''
+        _   -> run cpu'''
 
-incPc :: Cpu -> Word16 -> Cpu
-incPc cpu i = cpu{programCounter = programCounter cpu + i}
-
--- executeOpcode :: Cpu -> OpCode -> Cpu
--- executeOpcode cpu opBrk = brk (incPc cpu 1)
--- executeOpcode cpu 0xAA -> incPc 0 $ tax (incPc 1 cpu)
--- executeOpcode cpu 0xE8 -> incPc 0 $ inx (incPc 1 cpu)
--- executeOpcode cpu 0xA9 -> incPc 1 $ lda (incPc 1 cpu) Immediate
--- executeOpcode cpu 0xA5 -> incPc 1 $ lda (incPc 1 cpu) ZeroPage
--- executeOpcode cpu 0xB5 -> incPc 1 $ lda (incPc 1 cpu) ZeroPageX
--- executeOpcode cpu 0xAD -> incPc 2 $ lda (incPc 1 cpu) Absolute
--- executeOpcode cpu 0xBD -> incPc 2 $ lda (incPc 1 cpu) AbsoluteX
--- executeOpcode cpu 0xB9 -> incPc 2 $ lda (incPc 1 cpu) AbsoluteY
--- executeOpcode cpu 0xA1 -> incPc 1 $ lda (incPc 1 cpu) IndirectX
--- executeOpcode cpu 0xB1 -> incPc 1 $ lda (incPc 1 cpu) IndirectY
--- executeOpcode cpu 0x85 -> incPc 1 $ sta (incPc 1 cpu) ZeroPage
--- executeOpcode cpu 0x95 -> incPc 1 $ sta (incPc 1 cpu) ZeroPageX
--- executeOpcode cpu 0x8D -> incPc 2 $ sta (incPc 1 cpu) Absolute
--- executeOpcode cpu 0x9D -> incPc 2 $ sta (incPc 1 cpu) AbsoluteX
--- executeOpcode cpu 0x99 -> incPc 2 $ sta (incPc 1 cpu) AbsoluteY
--- executeOpcode cpu 0x81 -> incPc 1 $ sta (incPc 1 cpu) IndirectX
--- executeOpcode cpu 0x91 -> incPc 1 $ sta (incPc 1 cpu) IndirectY
--- executeOpcode cpu 0x69 -> incPc 1 $ adc (incPc 1 cpu) Immediate
--- executeOpcode cpu 0x65 -> incPc 1 $ adc (incPc 1 cpu) ZeroPage
--- executeOpcode cpu 0x75 -> incPc 1 $ adc (incPc 1 cpu) ZeroPageX
--- executeOpcode cpu 0x6D -> incPc 2 $ adc (incPc 1 cpu) Absolute
--- executeOpcode cpu 0x7D -> incPc 2 $ adc (incPc 1 cpu) AbsoluteX
--- executeOpcode cpu 0x79 -> incPc 2 $ adc (incPc 1 cpu) AbsoluteY
--- executeOpcode cpu 0x61 -> incPc 1 $ adc (incPc 1 cpu) IndirectX
--- executeOpcode cpu 0x71 -> incPc 1 $ adc (incPc 1 cpu) IndirectY
--- executeOpcode cpu 0xE9 -> incPc 1 $ sbc (incPc 1 cpu) Immediate
--- executeOpcode cpu 0xE5 -> incPc 1 $ sbc (incPc 1 cpu) ZeroPage
--- executeOpcode cpu 0xF5 -> incPc 1 $ sbc (incPc 1 cpu) ZeroPageX
--- executeOpcode cpu 0xED -> incPc 2 $ sbc (incPc 1 cpu) Absolute
--- executeOpcode cpu 0xFD -> incPc 2 $ sbc (incPc 1 cpu) AbsoluteX
--- executeOpcode cpu 0xF9 -> incPc 2 $ sbc (incPc 1 cpu) AbsoluteY
--- executeOpcode cpu 0xE1 -> incPc 1 $ sbc (incPc 1 cpu) IndirectX
--- executeOpcode cpu 0xF1 -> incPc 1 $ sbc (incPc 1 cpu) IndirectY
+incrementPc :: Cpu -> Word16 -> Cpu
+incrementPc cpu i = cpu{programCounter = programCounter cpu + i}
--- src/NesEmu/Cpu/Operations.hs
+++ src/NesEmu/Cpu/Operations.hs
@@ -1,11 +1,15 @@
-module NesEmu.Cpu.Operations where
+module NesEmu.Cpu.Operations (getOperation, getCycleCount) where
 
+import           Data.Word
 import           NesEmu.Cpu.Flags
 import           NesEmu.Cpu.Memory
+import           NesEmu.Cpu.Opcodes
 import           NesEmu.Cpu.Types
 
-lda :: Cpu -> AddressingMode -> Cpu
-lda cpu addrMode =
+type Operation = Cpu -> Cpu
+
+lda :: AddressingMode -> Cpu -> Cpu
+lda addrMode cpu =
     let addr = getOperandAddress cpu addrMode
         result = memoryRead cpu addr
         status' = status $ setZF result $ setNF result cpu
@@ -14,8 +18,8 @@ lda cpu addrMode =
             , status = status'
             }
 
-ldx :: Cpu -> AddressingMode -> Cpu
-ldx cpu addrMode =
+ldx :: AddressingMode -> Cpu -> Cpu
+ldx addrMode cpu =
     let addr = getOperandAddress cpu addrMode
         result = memoryRead cpu addr
         status' = status $ setZF result $ setNF result cpu
@@ -24,14 +28,14 @@ ldx cpu addrMode =
             , status = status'
             }
 
-sta :: Cpu -> AddressingMode -> Cpu
-sta cpu addrMode =
+sta :: AddressingMode -> Cpu -> Cpu
+sta addrMode cpu =
     let addr = getOperandAddress cpu addrMode
         val = registerA cpu
      in memoryWrite cpu addr val
 
-tax :: Cpu -> Cpu
-tax cpu =
+tax :: AddressingMode -> Cpu -> Cpu
+tax _ cpu =
     let result = registerA cpu
         status' = status $ setZF result $ setNF result cpu
      in cpu
@@ -39,8 +43,8 @@ tax cpu =
             , status = status'
             }
 
-adc :: Cpu -> AddressingMode -> Cpu
-adc cpu addrMode =
+adc :: AddressingMode -> Cpu -> Cpu
+adc addrMode cpu =
     let a = registerA cpu
         b = memoryRead cpu (getOperandAddress cpu addrMode)
         c = if getFlag Carry cpu then 1 else 0
@@ -51,8 +55,8 @@ adc cpu addrMode =
             , status = status'
             }
 
-sbc :: Cpu -> AddressingMode -> Cpu
-sbc cpu addrMode =
+sbc :: AddressingMode -> Cpu -> Cpu
+sbc addrMode cpu =
     let a = registerA cpu
         b = memoryRead cpu (getOperandAddress cpu addrMode)
         c = if getFlag Carry cpu then 1 else 0
@@ -63,8 +67,8 @@ sbc cpu addrMode =
             , status = status'
             }
 
-inx :: Cpu -> Cpu
-inx cpu =
+inx :: AddressingMode -> Cpu -> Cpu
+inx _ cpu =
     let result = registerX cpu + 1
         status' = status $ setZF result $ setNF result $ setVF result 1 0 cpu
      in cpu
@@ -72,5 +76,89 @@ inx cpu =
             , status = status'
             }
 
-brk :: Cpu -> Cpu
-brk cpu = cpu
+brk :: AddressingMode -> Cpu -> Cpu
+brk _ cpu = cpu
+
+getOperation :: OpCode -> Operation
+getOperation BRK          = brk NoneAddressing
+getOperation TAX          = tax NoneAddressing
+getOperation INX          = inx NoneAddressing
+getOperation LDAImmediate = lda Immediate
+getOperation LDAZeroPage  = lda ZeroPage
+getOperation LDAZeroPageX = lda ZeroPageX
+getOperation LDAAbsolute  = lda Absolute
+getOperation LDAAbsoluteX = lda AbsoluteX
+getOperation LDAAbsoluteY = lda AbsoluteY
+getOperation LDAIndirectX = lda IndirectX
+getOperation LDAIndirectY = lda IndirectY
+getOperation LDXImmediate = ldx Immediate
+getOperation LDXZeroPage  = ldx ZeroPage
+getOperation LDXZeroPageY = ldx ZeroPageY
+getOperation LDXAbsolute  = ldx Absolute
+getOperation LDXAbsoluteY = ldx AbsoluteY
+getOperation STAZeroPage  = sta ZeroPage
+getOperation STAZeroPageX = sta ZeroPageX
+getOperation STAAbsolute  = sta Absolute
+getOperation STAAbsoluteX = sta AbsoluteX
+getOperation STAAbsoluteY = sta AbsoluteY
+getOperation STAIndirectX = sta IndirectX
+getOperation STAIndirectY = sta IndirectY
+getOperation ADCImmediate = adc Immediate
+getOperation ADCZeroPage  = adc ZeroPage
+getOperation ADCZeroPageX = adc ZeroPageX
+getOperation ADCAbsolute  = adc Absolute
+getOperation ADCAbsoluteX = adc AbsoluteX
+getOperation ADCAbsoluteY = adc AbsoluteY
+getOperation ADCIndirectX = adc IndirectX
+getOperation ADCIndirectY = adc IndirectY
+getOperation SBCImmediate = sbc Immediate
+getOperation SBCZeroPage  = sbc ZeroPage
+getOperation SBCZeroPageX = sbc ZeroPageX
+getOperation SBCAbsolute  = sbc Absolute
+getOperation SBCAbsoluteX = sbc AbsoluteX
+getOperation SBCAbsoluteY = sbc AbsoluteY
+getOperation SBCIndirectX = sbc IndirectX
+getOperation SBCIndirectY = sbc IndirectY
+getOperation _            = error "Unknown opcode"
+
+getCycleCount :: OpCode -> Word16
+getCycleCount BRK          = 0
+getCycleCount TAX          = 0
+getCycleCount INX          = 0
+getCycleCount LDAImmediate = 1
+getCycleCount LDAZeroPage  = 1
+getCycleCount LDAZeroPageX = 1
+getCycleCount LDAAbsolute  = 2
+getCycleCount LDAAbsoluteX = 2
+getCycleCount LDAAbsoluteY = 2
+getCycleCount LDAIndirectX = 1
+getCycleCount LDAIndirectY = 1
+getCycleCount LDXImmediate = 1
+getCycleCount LDXZeroPage  = 1
+getCycleCount LDXZeroPageY = 1
+getCycleCount LDXAbsolute  = 2
+getCycleCount LDXAbsoluteY = 2
+getCycleCount STAZeroPage  = 1
+getCycleCount STAZeroPageX = 1
+getCycleCount STAAbsolute  = 2
+getCycleCount STAAbsoluteX = 2
+getCycleCount STAAbsoluteY = 2
+getCycleCount STAIndirectX = 1
+getCycleCount STAIndirectY = 1
+getCycleCount ADCImmediate = 1
+getCycleCount ADCZeroPage  = 1
+getCycleCount ADCZeroPageX = 1
+getCycleCount ADCAbsolute  = 2
+getCycleCount ADCAbsoluteX = 2
+getCycleCount ADCAbsoluteY = 2
+getCycleCount ADCIndirectX = 1
+getCycleCount ADCIndirectY = 1
+getCycleCount SBCImmediate = 1
+getCycleCount SBCZeroPage  = 1
+getCycleCount SBCZeroPageX = 1
+getCycleCount SBCAbsolute  = 2
+getCycleCount SBCAbsoluteX = 2
+getCycleCount SBCAbsoluteY = 2
+getCycleCount SBCIndirectX = 1
+getCycleCount SBCIndirectY = 1
+getCycleCount _            = error "Unknown opcode"
